75c4d4ddc7e272059ca2a3add8fec6a352cd2c3b
sullof <sullof@gmail.com>
2017-12-29 19:53:02 -0800

    Better tests

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -9,24 +9,27 @@ const fixtures = {
     badTweetId: '310462857140252672',
     tweetIdWrongSig: '410462857140252672',
     tweets: {
+        // good
         '210462857140252672': {
             text: 'Registering for 0xNIL IFO 0x8951ff6182aca2e98fe4a797fbfeec0a28c87aae2fe41a4110a5f0ec17d55b8615c4c73788e1b7b0c54ebf7cf60458b4948d93d85a8fc61b5de83b4285a141c401 :-)',
             user: {
                 screen_name: 'twitterapi'
             }
         },
+        // no sig
         '310462857140252672': {
-            text: 'Registering for 0xNIL IFO with wrong sig 0x8951ff6182aca2e98fe4a797fbfeec0a28c87aae2f :-)',
+            text: 'Registering for 0xNIL IFO without a sig :-)',
             user: {
                 screen_name: 'twitterapi'
             }
         },
+        // wrong sig
         '410462857140252672': {
             text: 'Registering for 0xNIL IFO 0x0000000000aca2e98fe4a797fbfeec0a28c87aae2fe41a4110a5f0ec17d55b8615c4c73788e1b7b0c54ebf7cf60458b4948d93d85a8fc61b5de83b4285a141c401 :-)',
             user: {
                 screen_name: 'twitterapi'
             }
-        },
+        }
     }
 }
 
