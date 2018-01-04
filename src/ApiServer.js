a4b23f8d842cb296eca3df053a3d007316816ed3
sullof <sullof@gmail.com>
2018-01-03 17:25:38 -0800

    Better async management of apiServer

--- src/ApiServer.js
+++ src/ApiServer.js
@@ -1,33 +1,18 @@
 const http = require('http')
 
 const app = require('./app')
-const log = require('./lib/log')
+const Logger = require('./lib/Logger')
+
+const noop = new Function()
 
 class ApiServer {
 
-  constructor(port) {
+  constructor(port = 9292) {
 
-    this.port = this.normalizePort(port)
+    this.port = port
     app.set('port', this.port)
-    this.server = http.createServer(app)
-  }
 
-  normalizePort(val) {
-    const port = parseInt(val, 10)
-    if (isNaN(port)) {
-      return val
-    }
-    if (port >= 0) {
-      return port
-    }
-    return false
-  }
-
-  start(callback = new Function()) {
-
-    this.server.listen(this.port, callback)
-
-    this.server.on('error', error => {
+    app.on('error', error => {
       if (error.syscall !== 'listen') {
         throw error
       }
@@ -52,24 +37,25 @@ class ApiServer {
       }
       /* eslint-enable */
     })
+  }
 
-    this.server.on('listening', () => {
-      const addr = this.server.address()
-      const bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port
-      this.listening = true
-      log.debug('Listening on ' + bind)
+  start(callback = noop) {
 
+    this.server = app.listen(this.port, () => {
+      Logger.info(`Test server listening on port ${ this.port }`)
+      callback()
     })
-
   }
 
-  stop() {
-    this.server.close()
-    this.listening = false
+  stop(done = noop) {
+    if (this.server) {
+      this.server.close(() => {
+        done()
+      })
+    } else {
+      done()
+    }
   }
-
 }
 
 module.exports = ApiServer
