574e9fdc8c8d67aeec663cf36494ca0ee4a45569
sullof <sullof@gmail.com>
2018-03-05 13:25:38 -0800

    Trying adding weibo. Does not work.

--- index.js
+++ index.js
@@ -27,16 +27,16 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
 
-  const [tmp, screenName, id, address] = pathname.split('/')
+  const [tmp, app, screenName, id, address] = pathname.split('/')
 
   function respond(err, val) {
     log(val || '0', err || '')
     res.end(val || '')
   }
 
-  if (id && /^[a-z0-9_]+$/i.test(screenName) && screenName.length <= 15 && /^\d{18,20}$/.test(id) && id.length < 20 && web3.utils.isAddress(address)) {
+  if (app == 't' && id && /^[a-z0-9_]+$/i.test(screenName) && screenName.length <= 15 && /^\d{18,20}$/.test(id) && id.length < 20 && web3.utils.isAddress(address)) {
 
-    log('GET ' + pathname)
+    log('GET 0 ' + pathname)
 
     res.setHeader('Access-Control-Allow-Origin', '*')
 
@@ -49,7 +49,7 @@ const server = http.createServer((req, res) => {
         const userId = $('div[data-tweet-id="'+id+'"]').attr('data-user-id')
         if (/^0x[0-9a-f]{130}/.test(content) && /^\w+$/.test(userId)) {
 
-          const msgHash = utils.hashPersonalMessage(utils.toBuffer(`${screenName}@tweedentity`))
+          const msgHash = utils.hashPersonalMessage(utils.toBuffer(`twitter/${userId}@tweedentity`))
           const sgn = utils.stripHexPrefix(content)
           const r = new Buffer(sgn.slice(0, 64), 'hex')
           const s = new Buffer(sgn.slice(64, 128), 'hex')
@@ -61,6 +61,41 @@ const server = http.createServer((req, res) => {
             respond(null, userId)
           } else respond('wrong-sig') // wrong signature
         } else respond('wrong-tweet') // wrong tweet
+
+      }
+    })
+    .catch(function (err) {
+      log('ERROR ' + err)
+      respond('catch-error')
+    })
+
+  } else if (app == 'w' && id && /^\d+$/i.test(screenName) && screenName.length <= 15 && /^[a-z0-9_]+$/.test(id) && id.length < 12 && web3.utils.isAddress(address)) {
+
+    log('GET 1 ' + pathname)
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    request
+    .get(`https://weibo.com/${screenName}/${id}`)
+    .then(function (tweet) {
+      if (tweet.text) {
+        const $ = cheerio.load(tweet.text)
+        const content = $('meta[name="description"]').attr('content').replace(/[^x0-9a-f]+/g,'')
+        const userId = screenName
+        if (/^0x[0-9a-f]{130}/.test(content) && /^\w+$/.test(userId)) {
+
+          const msgHash = utils.hashPersonalMessage(utils.toBuffer(`weibo/${userId}@tweedentity`))
+          const sgn = utils.stripHexPrefix(content)
+          const r = new Buffer(sgn.slice(0, 64), 'hex')
+          const s = new Buffer(sgn.slice(64, 128), 'hex')
+          const v = parseInt(sgn.slice(128, 130), 16) // + 27
+          const pub = utils.ecrecover(msgHash, v, r, s)
+          const addr = utils.setLength(utils.fromSigned(utils.pubToAddress(pub)), 20)
+
+          if (utils.bufferToHex(addr).toLowerCase() === address.toLowerCase()) {
+            respond(null, userId)
+          } else respond('wrong-sig') // wrong signature
+        } else respond('wrong-post') // wrong tweet
 
       }
     })
