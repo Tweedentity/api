3512a3ed18a5f72c0f22e10caf241e93849e2c8d
louisbuchbinder <louisbuchbinder@gmail.com>
2018-04-20 19:28:46 -0700

    test /tweeter/:photoId

--- test/index.js
+++ test/index.js
@@ -1,12 +1,62 @@
+const sinon = require('sinon');
 const request = require('supertest');
+const db = require('../db');
 const {app} = require('../');
 
 describe('Tweedentity Api integration tests', function () {
+	let dbGet;
+
+	before(function () {
+		dbGet = sinon.stub(db, 'get');
+
+	});
+
+	beforeEach(function () {
+		dbGet.callsFake((_, cb) => cb(null, 'fake'));
+	});
+
+	afterEach(function () {
+		dbGet.reset();
+	});
+
+	after(function () {
+		dbGet.restore();
+	});
 
 	it('should respond 200 for GET /', function () {
 		return request(app)
 			.get('/')
 			.expect(200);
 	});
+
+	it('should respond 200 for GET /tweeter/:userId', function () {
+		return request(app)
+			.get('/tweeter/1234')
+			.expect(200);
+	});
+
+	it('should respond 400 for db.get error', function () {
+		dbGet.callsFake((_, cb) => cb(new Error('fake')));
+
+		return request(app)
+			.get('/tweeter/1234')
+			.expect(400);
+	});
+
+	it('should call db.get on the userId', function () {
+		return request(app)
+			.get('/tweeter/1234')
+			.then(() => {
+				sinon.assert.calledWith(dbGet, '1234');
+			});
+	});
+
+	it('should respond with json', function () {
+		return request(app)
+			.get('/tweeter/1234')
+			.expect('Content-Type', 'application/json; charset=utf-8')
+			.expect(JSON.stringify('fake'));
+	});
+
 
 });
