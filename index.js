65ec3c04a58fc5dc7159cc4a42bed4cf379db66f
sullof <sullof@gmail.com>
2018-05-28 19:07:26 -0700

    Adding web3 format for personal signatures

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const request = require('superagent')
-const utils = require('ethereumjs-util')
+const ethUtil = require('ethereumjs-util')
 const cheerio = require('cheerio')
 const serverless = require('serverless-http')
 
 const express = require('express')
 const app = express()
 
+const utils = require('./lib/Utils')
 const db = require('./db')
 
 app.get('/', (req, res) => {
@@ -43,44 +44,25 @@ app.get('/tweet/:tweetId/:address', (req, res) => {
       if (tweet.text) {
         const $ = cheerio.load(tweet.text)
 
-        const tmp = $('meta[property="og:description"]').attr('content').replace(/^(|.+)tweedentity\(/, '').replace(/\)(|.+)$/,'').split(';')
-
-        const content = tmp[0].split(',')
-        const meta = tmp[1].split(',')
-
         const dataTweet = $('div[data-tweet-id="' + tweetId + '"]')
         const userId = dataTweet.attr('data-user-id')
         const screenName = dataTweet.attr('data-screen-name')
         const name = dataTweet.attr('data-name')
 
-        const message = `twitter/${userId}@tweedentity`
-        const address0 = content[0].toLowerCase()
-        const message0 = `${content[1]}@tweedentity`
-        const sig = content[2]
-        const sigVer = content[3]
-        const signer = content[4].split(':')[0]
-        const version = meta[0]
+        const data = utils.deconstructTweet($('meta[property="og:description"]').attr('content'))
 
-        if (version === '1' && address0 === address.substring(0,4) && /^\w+$/.test(userId) && message0 === message && /^0x[0-9a-f]{130}/.test(sig)) {
+        const {shortAddr, message, sig, signer, signame, version} = data
 
-          const msgHash = utils.hashPersonalMessage(utils.toBuffer(message))
-          const sgn = utils.stripHexPrefix(sig)
-          const r = new Buffer(sgn.slice(0, 64), 'hex')
-          const s = new Buffer(sgn.slice(64, 128), 'hex')
-          let v = parseInt(sgn.slice(128, 130), 16)
-          if (v < 27) {
-            v += 27
-          }
-          const pub = utils.ecrecover(msgHash, v, r, s)
-          const addr = utils.setLength(utils.fromSigned(utils.pubToAddress(pub)), 20)
-          if (utils.bufferToHex(addr).toLowerCase() === address.toLowerCase()) {
+        if (version === '1' && shortAddr === address.substring(0, 4) && /^\w+$/.test(userId) && message === `twitter/${userId}@tweedentity` && /^0x[0-9a-f]{130}/.test(sig)) {
+
+          if (utils.verify(address, message, sig, signer, signame)) {
             db.put(userId, screenName, name, address, (err) => {
               if (err) {
                 console.log('Error', err)
               }
               respond(null, userId)
             })
-          } else respond('wrong-sig') // wrong signature
+          } else respond('wrong-sig') // wrong utils
         } else respond('wrong-tweet') // wrong tweet
 
       }
